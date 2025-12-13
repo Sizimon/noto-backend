1a06dc3c891fbc16f555ed867e42288f5be0430e
Sizimon <siizmonsus@gmail.com>
2025-12-13 22:26:03 +0100

    FIX: Dbconnection wrong variables

--- db/dbConnection.ts
+++ db/dbConnection.ts
@@ -5,11 +5,11 @@ dotenv.config();
 pg.types.setTypeParser(20, val => Number(val)); // PSQL sends Bigints as strings, this ensures it always converts bigints to actual numbers > 20 is the OID for BIGINT
 
 const pool = new Pool({
-    user: process.env.NOTO_DB_USER,
+    user: process.env.DB_USER,
     host: process.env.NOTO_DB_HOST,
-    database: process.env.NOTO_DB_NAME,
-    password: process.env.NOTO_DB_PASSWORD,
-    port: Number(process.env.NOTO_DB_PORT) || 5432, // Default to 5432 if not set
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.NOTO_DB_PORT ? Number(process.env.NOTO_DB_PORT) : 5432, // Default to 5432 if not set
 });
 
 export default pool;
